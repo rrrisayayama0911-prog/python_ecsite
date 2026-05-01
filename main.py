e8bf6a2af7974a201b4ffee21ca02f2d65fc1712
risaoya <rrrisayayama0911@gmail.com>
2026-05-02 08:05:29 +0900

    ②接続確認(バックエンド→フロントエンド)

--- main.py
+++ main.py
@@ -12,9 +12,20 @@ templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 # メイン画面（ここからが②接続確認のメイン！）
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    # ここに商品データを書いていく
-    return templates.TemplateResponse(request=request, name="index.html")
-
+    # 1.ここにHTMLに送るための「データの箱」を作る
+    
+    items = [
+        {"name":"チョコレート","price":300}, # 辞書形式
+        {"name":"バニラ","price":250},
+        {"name":"ストロベリー","price":320},
+        {"name":"抹茶","price":280}
+    ]
+    # 2. テンプレートエンジン(Jinja2)にデータを渡して出荷
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"items":items} #ここでデータをHTMLに流す。
+    )
 # テスト用API
 @app.get("/test")
 def show_test():
