b0b3e6617cba4912bb80bbc5dd9ed28a67d6de72
risaoya <rrrisayayama0911@gmail.com>
2026-05-12 08:08:18 +0900

    ⑦DBのidメールアドレスでPW追加

--- init.sql
+++ init.sql
@@ -1,14 +1,13 @@
 -- ユーザー情報を管理する扉（テーブル）
--- 既にテーブルがある場合は作成しない（IF NOT EXISTS）
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,              -- 1, 2, 3...と自動で振られる背番号（鍵穴）
-    email VARCHAR(255) UNIQUE NOT NULL, -- 重複を許さないメールアドレス（二重ロック）
+CREATE TABLE users (
+    email TEXT PRIMARY KEY, -- メールアドレスをIDにする
+	password TEXT NOT NULL,-- パスワード（今回は平文）
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- 登録日時（いつ作られたかの証跡）
 );
 
--- コメント：
--- SERIAL は PostgreSQL 独自のデータ型で、自動で連番を振ってくれます。
--- UNIQUE をつけることで、同じメールアドレスでの二重登録（バグ）を防ぎます。
+-- テスト用のデータを1つ入れておく
+INSERT INTO users (email, password) VALUES ('test@example.com', 'pass123');
+
 
 
 -- 商品情報を管理する扉（テーブル）
